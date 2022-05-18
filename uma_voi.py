a5b92a9c8f779e3b2009523d6acba9209b924ce5
Yukari-Lily <937334498@qq.com>
2022-05-18 14:15:31 +0800

    1

--- uma_voi.py
+++ uma_voi.py
@@ -277,9 +277,8 @@ async def muli(bot,ev) -> MessageSegment:
 #语音部分
 @sv.on_keyword("特别渣", "特别肥", "特别胖", "特别能吃", "小特", "斯佩酱", "斯佩夏尔维特")
 async def muli(bot,ev) -> MessageSegment:
-    file = get_1001_folder
     try:
-        rec = MessageSegment.record(f'file:///{os.path.abspath(file.path)}')
+        rec = MessageSegment.record(f'file:///{os.path.abspath(get_1001_folder.path)}')
         await bot.send(ev,rec)
     except CQHttpError:
         sv.logger.erro("发送失败喵")
